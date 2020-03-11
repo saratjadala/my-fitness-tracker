8d280f2cb29e4ff032ccf677fdafab6222dcb1b2
saratjadala <js.prince96@gmail.com>
2020-03-11 16:01:25 -0400

    commit

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,13 @@
-// ...
-import './../node_modules/bulma/css/bulma.css';
-// ...
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bulma';
+import '@fortawesome/fontawesome-free/css/all.css'
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#App')

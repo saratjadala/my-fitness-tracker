47f775607f4df612b6264b98dc2b521ec10be806
saratjadala <js.prince96@gmail.com>
2020-03-11 16:54:09 -0400

    commit

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,7 @@
-
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bulma';
-import '@fortawesome/fontawesome-free/css/all.css'
+import './../node_modules/bulma/css/bulma.css';
 
 Vue.config.productionTip = false
 

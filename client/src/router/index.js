462c222c8a7b85e908c6acae781cb48ac5be4812
saratjadala <js.prince96@gmail.com>
2020-03-12 18:35:33 -0400

    commit

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -21,15 +21,19 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/login ',
-    name: 'login',
-    component: login
-  },
-  {
-    path: '/signup ',
+    path: '/signup',
     name: 'signup',
-
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: signup
+    },  {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: login
   }
 ]
 

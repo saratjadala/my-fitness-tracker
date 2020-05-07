fb5e2e6805db8874ac3cb00e2f386f576d2eda46
saratjadala <js.prince96@gmail.com>
2020-05-07 17:03:33 -0400

    commit

--- client/src/models/users.js
+++ client/src/models/users.js
@@ -1,11 +1,11 @@
 /* B"H
 */
 import myFetch from "./myFetch";
-import login from "./login";
+
 
 export let CurrentUser = null;
 
-export async function login(email, password) {
+export async function Login(email, password) {
 
     const user = await myFetch('/Users/login', { email, password }) ;
 
